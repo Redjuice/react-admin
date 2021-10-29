b5d5d7346588f5ac98a7dfebb80a09d774a3ced9
wangheng <wangheng@vchangyi.com>
2021-10-29 17:42:46 +0800

    feat(craco): 配置craco(antd按需加载, 别名, less)

---
+++ craco.config.js
@@ -0,0 +1,39 @@
+const CracoLessPlugin = require("craco-less");
+const path = require("path");
+
+module.exports = {
+  babel: {
+    plugins: [
+      // 配置 babel-plugin-import, 解决 AntDesign 按需加载
+      [
+        "import",
+        {
+          libraryName: "antd",
+          libraryDirectory: "es",
+          style: "css",
+        },
+        "antd",
+      ],
+    ],
+  },
+  webpack: {
+    // 配置别名
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  // craco 提供的插件
+  plugins: [
+    // 配置 less
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from '@/logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -6,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Hello Antd</Button>
       </header>
     </div>
   );
