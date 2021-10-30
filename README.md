cfd1c3c983876d6b2bd18f8056d28d647abff9cc
wangheng <wangheng@vchangyi.com>
2021-10-30 17:00:56 +0800

    feat(css): reaset-css+全局类名+less模块化+vscode保存自动格式化

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-admin
-基于React+antd实现的后台模板
+
+基于 React+antd 实现的后台模板
+
+- styleint, 实现 css 格式化
+
+- less 模块化, 解决类名全局污染问题 https://blog.csdn.net/mpr0xy/article/details/103512652
+
+- less 全局变量
--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from '@/logo.svg'
 import { Button } from 'antd'
-import './App.less'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Button type="primary">Hello Antd</Button>
       </header>
     </div>
