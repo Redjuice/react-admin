7fe9a9728745fa1edf7634e20e54e3478fe02a06
wangheng <wangheng@vchangyi.com>
2021-10-31 14:15:10 +0800

    feat(业务): 登录相关

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,13 +1,91 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Form, Input, Button, message } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { title } from '@/config'
+import { loginApi } from '@/apis'
+import { saveUserAndToken } from '@/redux/actions/login'
+import './index.less'
 
-@connect((state) => ({ isLogin: state.login.isLogin }))
+@connect((state) => ({ isLogin: state.login.isLogin }), {
+  saveUserAndToken
+})
 class Login extends Component {
-  componentDidMount() {
-    console.log(this.props)
+  login = async (values) => {
+    const { status, msg, data } = await loginApi(values)
+    if (status === 0) {
+      const { user, token } = data
+      this.props.saveUserAndToken({ user, token }) // 保存数据到redux
+      message.info('登录成功')
+      this.props.history.replace('/')
+    } else {
+      message.warn(msg)
+    }
   }
+
+  validator = (_, value) => {
+    const length = value && value.length
+    const pwdReg = /^[a-zA-Z0-9_]+$/
+    if (!value) {
+      return Promise.reject(new Error('必须输入密码'))
+    } else if (length < 4) {
+      return Promise.reject(new Error('密码必须大于 4 位'))
+    } else if (length > 12) {
+      return Promise.reject(new Error('密码必须小于 12 位'))
+    } else if (!pwdReg.test(value)) {
+      return Promise.reject(new Error('密码必须是英文、数组或下划线组成'))
+    } else {
+      return Promise.resolve()
+    }
+  }
+
   render() {
-    return <div>Login</div>
+    const { isLogin } = this.props
+    if (isLogin) return <Redirect to="/" />
+    return (
+      <div className="login" data-component="login">
+        <section className="login-content">
+          <div className="title">{title}</div>
+          <Form onFinish={this.login} className="login-form">
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, whitespace: true, message: '必须输入用户名' },
+                { min: 4, message: '用户名必须大于 4 位' },
+                { max: 12, message: '用户名必须小于 12 位' },
+                {
+                  pattern: /^[a-zA-Z0-9_]+$/,
+                  message: '用户名必须是英文、数组或下划线组成'
+                }
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="login-form-icon" />}
+                placeholder="用户名"
+              />
+            </Form.Item>
+            <Form.Item name="password" rules={[{ validator: this.validator }]}>
+              <Input
+                prefix={<LockOutlined className="login-form-icon" />}
+                type="password"
+                placeholder="密码"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                size="large"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    )
   }
 }
 
